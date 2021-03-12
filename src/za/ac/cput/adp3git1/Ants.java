1c62fecfc3364df3db6b14735a79a714bfc813a8
Andy Hine <219259038@mycput.ac.za>
2021-03-12 19:55:16 +0200

    included a comment.

--- src/za/ac/cput/adp3git1/Ants.java
+++ src/za/ac/cput/adp3git1/Ants.java
@@ -1,6 +1,12 @@
 package za.ac.cput.adp3git1;
 
 public class Ants {
+    /**
+     *
+     * @param str
+     * @return
+     */
+
     public int lengthOfString(String str){
         return str.length();
     }
